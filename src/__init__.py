e8f03145c9ce4252224347ededf98a7dd3f6a7d6
shivraj1182 <79820642+shivraj1182@users.noreply.github.com>
2025-12-27 18:00:52 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,26 @@
+"""FinanceInsight Package
+
+A comprehensive NER system for financial data extraction from unstructured documents.
+"""
+
+__version__ = "0.1.0"
+__author__ = "shivraj1182"
+__description__ = "Named Entity Recognition for Financial Data Extraction"
+
+from .data_preparation import DataPreparation
+from .ner_model import BERTBasedNER, FinBERTNER, NERTrainer, ModelEvaluator
+from .entity_extractor import FinancialEntityExtractor, EntityLinker
+from .document_parser import FinancialDocumentParser, TableParser, DocumentStructureAnalyzer
+
+__all__ = [
+    'DataPreparation',
+    'BERTBasedNER',
+    'FinBERTNER',
+    'NERTrainer',
+    'ModelEvaluator',
+    'FinancialEntityExtractor',
+    'EntityLinker',
+    'FinancialDocumentParser',
+    'TableParser',
+    'DocumentStructureAnalyzer'
+]
