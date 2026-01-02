8dbe7b50dbdec6161926282381eb0edb02065c26
shivraj1182 <79820642+shivraj1182@users.noreply.github.com>
2026-01-02 21:55:04 +0530

    Update __init__.pyFix: Remove EntityLinker import and reference from __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,13 @@
 """FinanceInsight Package
-
 A comprehensive NER system for financial data extraction from unstructured documents.
 """
-
 __version__ = "0.1.0"
 __author__ = "shivraj1182"
 __description__ = "Named Entity Recognition for Financial Data Extraction"
 
 from .data_preparation import DataPreparation
 from .ner_model import BERTBasedNER, FinBERTNER, NERTrainer, ModelEvaluator
-from .entity_extractor import FinancialEntityExtractor, EntityLinker
+from .entity_extractor import FinancialEntityExtractor
 from .document_parser import FinancialDocumentParser, TableParser, DocumentStructureAnalyzer
 
 __all__ = [
@@ -19,7 +17,6 @@ __all__ = [
     'NERTrainer',
     'ModelEvaluator',
     'FinancialEntityExtractor',
-    'EntityLinker',
     'FinancialDocumentParser',
     'TableParser',
     'DocumentStructureAnalyzer'
